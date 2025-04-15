fea3ab7d9f49d9704716ca02287439b2360eac48
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-04-15 20:00:18 +0530

    feat: new solution added

--- Leetcode Solutions/Completed/1.java
+++ Leetcode Solutions/Completed/1.java
@@ -1,17 +1,27 @@
+import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        int [] twoSum = new int [2];
-        
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    twoSum[0] = i;
-                    twoSum[1] = j;
-                }
+        // Optimal Approach  O(N)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(target-nums[i])) {
+                return new int[] {i, map.get(target-nums[i])};
             }
+            map.put(nums[i], i);
         }
+        return new int[] {-1};
 
-        return twoSum;
+
+
+
+        // Brute Force Approach  O(N^2)
+        // for (int i=0; i<nums.length; i++) {
+        //     for (int j=i+1; j<nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[] {i, j};
+        //         }
+        //     }
+        // }
+        // return new int[] {-1, -1};
     }
 }
